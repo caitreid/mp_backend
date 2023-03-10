3c705e6a10b986b7ae816ff4b2bd69423c0be17b
caitreid <ctlnrd@gmail.com>
2023-03-10 17:41:28 -0500

    wip profile views and urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.mango_views import Mangos, MangoDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
+from .views.profile_views import Profiles, ProfileDetail
 
 urlpatterns = [
   	# Restful routing
@@ -9,5 +10,7 @@ urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
-    path('change-pw/', ChangePassword.as_view(), name='change-pw')
+    path('change-pw/', ChangePassword.as_view(), name='change-pw'),
+    path('/profile/create/', Profiles.as_view(), name='profiles'),
+    path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile_detail')
 ]
