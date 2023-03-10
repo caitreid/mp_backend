b02d4fe5bebc87f6d3760b8329ba9a74de6db4ef
caitreid <ctlnrd@gmail.com>
2023-03-10 17:41:09 -0500

    profile serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,18 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models.mango import Mango
+from .models.profile import Profile
 from .models.user import User
 
 class MangoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mango
         fields = ('id', 'name', 'color', 'ripe', 'owner')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'username', 'title', 'bio', 'visible', 'owner')
 
 class UserSerializer(serializers.ModelSerializer):
     # This model serializer will be used for User creation
